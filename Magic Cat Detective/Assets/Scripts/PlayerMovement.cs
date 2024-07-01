ec9c063b1fa2849a7685b7fbdad065494fdf6349
ZenBowman <PrasanSamtani@gmail.com>
2024-06-30 18:19:41 -0700

    Add basic movement

--- Magic Cat Detective/Assets/Scripts/PlayerMovement.cs
+++ Magic Cat Detective/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private const float kSpeed = 0.1f;
+
+    private const float kRotationSpeed = 1.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,17 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         
+        float x_direction = Input.GetAxis("Horizontal");
+        float z_direction = Input.GetAxis("Vertical");
+
+        Vector3 move_direction = new Vector3(x_direction, 0.0f, z_direction) * kSpeed;
+
+        transform.Translate(move_direction);
+        
+        
+        float h = kRotationSpeed * Input.GetAxis("Mouse X");
+        float v = kRotationSpeed * Input.GetAxis("Mouse Y");
+
+        transform.Rotate(0, h, 0);
     }
 }
